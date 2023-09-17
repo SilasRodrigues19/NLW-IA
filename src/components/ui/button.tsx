5a3c841b539cf1855cd0bb1d64bc0cc9bfe89959
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2023-09-16 21:09:12 -0300

    refactor: add new variant to button component

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -11,6 +11,8 @@ const buttonVariants = cva(
       variant: {
         default:
           "bg-primary text-primary-foreground shadow hover:bg-primary/90",
+        inherit:
+          "bg-transparent text-muted-foreground shadow-none",
         destructive:
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
