6628464537b5e9732eeed251d2b4c968f474238e
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2023-09-17 16:56:51 -0300

    refactor: add dynamic class to alert component

--- src/components/alert-dialog.tsx
+++ src/components/alert-dialog.tsx
@@ -3,15 +3,22 @@ import { Alert, AlertTitle } from "../components";
 interface AlertDialogProps {
   icon: React.ElementType;
   title: string;
+  className?: string;
 }
 
-export const AlertDialog = ({icon: Icon, title}: AlertDialogProps) => {
+export const AlertDialog = ({icon: Icon, title, className}: AlertDialogProps) => {
   return (
-      <Alert className="bg-rose-500 text-foreground pointer-events-none select-none">
-        <AlertTitle className='flex items-center justify-center gap-3 w-full'>
-          <Icon className='flex h-4 w-4' />
-          {title}
-        </AlertTitle>
-      </Alert>
+    <Alert
+      className={
+        className
+          ? `${className} bg-transparent`
+          : 'bg-rose-500 text-foreground pointer-events-none select-none'
+      }
+    >
+      <AlertTitle className='flex items-center justify-center gap-3 w-full'>
+        <Icon className='flex h-4 w-4' />
+        {title}
+      </AlertTitle>
+    </Alert>
   );
 }
